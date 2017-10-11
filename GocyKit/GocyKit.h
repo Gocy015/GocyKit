25d5804af63ca4ad5d63a8d84d947ad01fa9a74b
Gocy <caiyang@yy.com>
2017-10-11 10:57:40 +0800

    Initial Commit

---
+++ GocyKit/GocyKit.h
@@ -0,0 +1,19 @@
+//
+//  GocyKit.h
+//  GocyKit
+//
+//  Created by Gocy on 2017/10/11.
+//  Copyright © 2017年 Gocy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GocyKit.
+FOUNDATION_EXPORT double GocyKitVersionNumber;
+
+//! Project version string for GocyKit.
+FOUNDATION_EXPORT const unsigned char GocyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GocyKit/PublicHeader.h>
+
+
